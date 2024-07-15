ccbcddcce91b86da9f74d03625b5fb20d9914f9f
Fisehatsion Adisu Abute <fisehatsionadisu@gmail.com>
2024-07-15 13:09:58 +0300

    Update recruitment accepter.py

--- recruitment accepter.py
+++ recruitment accepter.py
@@ -18,3 +18,11 @@ if x >= 1 and x <= 30:
 else:
     print("Invalid input. Please enter a day between 1 and 30.")
 print("https://fisehatsion-adisu-abute.hi.link")
+
+sports = [ "Football", "Basketball", "Tennis", "Golf", "Volleyball"]
+group = [x for x in sports if "ball" in x]
+print(group)
+
+words = ["tree", "button", "cat", "window", "defenestrate"]
+required = [x for x in words if len(x) > 4 ]
+print(required)
