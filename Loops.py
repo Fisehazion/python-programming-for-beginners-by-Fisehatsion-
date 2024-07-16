55b43d4883a988ed5354994cae9e8e8b6f42195d
Fisehatsion Adisu Abute <fisehatsionadisu@gmail.com>
2024-07-16 17:19:08 +0300

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,1 +1,8 @@
+sports = [ "Football", "Basketball", "Tennis", "Golf", "Volleyball"]
+group = [x for x in sports if "ball" in x]
+print(group)
+
+words = ["tree", "button", "cat", "window", "defenestrate"]
+required = [x for x in words if len(x) > 4 ]
+print(required)
 
